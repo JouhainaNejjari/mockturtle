63e34342ad97417bbda30825229ac88e01161f9d
JouhainaNejjari <95933906+JouhainaNejjari@users.noreply.github.com>
2021-12-24 02:55:05 +0100

    Update simulation_cec.hpp

--- include/mockturtle/algorithms/simulation_cec.hpp
+++ include/mockturtle/algorithms/simulation_cec.hpp
@@ -26,7 +26,6 @@
 /*!
   \file simulation_cec.hpp
   \brief Simulation-based CEC
-
   EPFL CS-472 2021 Final Project Option 2
 */
 
@@ -39,6 +38,7 @@
 #include "../utils/node_map.hpp"
 #include "miter.hpp"
 #include "simulation.hpp"
+#include <cmath>
 
 namespace mockturtle
 {
@@ -55,6 +55,13 @@ struct simulation_cec_stats
 
 namespace detail
 {
+
+/*! \brief Simulates truth tables in rounds with variable limit.
+ *
+ * This simulator simulates truth tables.  Each primary input is assigned the
+ * projection function according to the index.  The number of variables be
+ * passed to the constructor of the simulator.
+ */
 
 template<class Ntk>
 class simulation_cec_impl
@@ -74,11 +81,125 @@ public:
   bool run()
   {
     /* TODO: write your implementation here */
-    return false;
+
+    // Computing split_var and rounds
+    uint32_t n = _ntk.num_pis();
+    uint32_t split_var = compute_splitting_var(n);
+    uint32_t rounds = compute_rounds(n, split_var);
+
+    // Reporting split_var and rounds to the user as statistics
+    _st.split_var = split_var;
+    _st.rounds = rounds;
+
+    // Initializing pattern
+    pattern_t patterns = init_patterns(_ntk, split_var);
+
+    // Simulating first round
+    default_simulator<kitty::dynamic_truth_table> sim(split_var);
+    simulate_nodes(_ntk, patterns, sim);
+
+    // Checking patterns
+    if (!check( _ntk,patterns )){
+      return(false);
+    }
+
+    // Looping over simulation rounds
+    for (uint32_t round = 1; round < rounds; round++) {
+      update_pattern(round, patterns,split_var);
+      simulate_nodes(_ntk, patterns, sim);
+
+      // Pattern checking
+      if (!check( _ntk,patterns )){
+          return(false);
+          }
+    }
+
+    return true;
   }
 
 private:
   /* you can add additional methods here */
+  uint32_t compute_splitting_var(uint32_t n) {
+      uint32_t split_var;
+      if (n<=6)
+        split_var=n;
+      else {
+          uint32_t v = _ntk.size();
+          uint32_t m = 7;
+          uint32_t cond = ( 32 + (1 << (m-3)) )*v;
+          uint32_t max = 1 << 29;
+          while (m<=n && cond<=max)
+            m++;
+          split_var = m;
+      }
+      return split_var;
+  }
+
+  uint32_t compute_rounds(uint32_t n, uint32_t split_var) {
+      return 1 << (n-split_var);
+  }
+
+  pattern_t init_patterns( Ntk& _N, uint32_t split_var){
+      pattern_t patterns(_N);
+
+      _N.foreach_pi( [&]( auto const& n, auto p ){
+       kitty::dynamic_truth_table tt (split_var);
+       if (p < split_var) {
+       kitty:: create_nth_var(tt , p);
+       }
+       patterns[n]=tt;
+    } );
+    return patterns;
+  }
+
+  bool check (Ntk& _N ,pattern_t& patterns){
+    bool eq = true;
+    _N.foreach_po( [&]( auto const& f) {
+      if ( _N.is_complemented( f ) )
+      {
+        if ( !is_const0(~patterns[f]) ) {
+          eq = false;
+        }
+      }
+      else
+      {
+        if ( !is_const0(patterns[f]) ) {
+        eq = false;
+        }
+      }
+    } );
+    return eq;
+  }
+
+  /*the function to update the pattern*/
+  void update_pattern( uint32_t round , pattern_t& patterns, uint32_t split_var ){
+    // Cleaning old patterns
+    _ntk.foreach_gate( [&]( auto const& m )
+    {
+       patterns.erase(m);
+    } );
+
+    uint32_t r = round;
+    // Updating patterns
+      _ntk.foreach_pi( [&]( auto const& n, auto i )
+      {
+        // Splitting variables for indices after split_var
+        if (i >= split_var ){
+          // Case where round is odd
+          if (r % 2 == 1) {
+            // Updating patterns
+            if ( is_const0(patterns[n]) ) patterns[n] = ~patterns[n];
+          }
+        // Case where round is even
+        else {
+          // Updating patterns
+          if ( !is_const0(patterns[n]) ) patterns[n] = ~patterns[n];
+        }
+        r /= 2;
+
+        }
+      } );
+  }
 
 private:
   Ntk& _ntk;
